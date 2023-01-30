b0591a30ede84a95f29bf76eb7e27b2f8a30d062
Daniel Shelestov <fellowers.flobot@gmail.com>
2023-01-30 16:44:27 +0300

    add history of moves container
    - add history to top level from Board to Game
    - fix minor issues with renderSquare method
    - move handleClick on the stage up
    - rewrite handleClick for history of moves

--- src/index.tsx
+++ src/index.tsx
@@ -15,50 +15,24 @@ function Square({value, onClick}: SquareProps) {
   );
 }
 
-interface BoardProps {}
+interface BoardProps {
+  history: Array<{squares: string[]}>
+  onClick: (i: number) => void
+}
 
 class Board extends React.Component<BoardProps, {squares: string[], xIsNext: boolean}> {
-  constructor(props: BoardProps) {
-    super(props);
-    this.state = {
-      squares: Array<string>(9).fill(''),
-      xIsNext: true
-    };
-  }
-
   renderSquare(i: number) {
     return (
     <Square
-    value={this.state.squares[i]}
-    onClick={() => this.handleClick(i)}
+    value={this.props.history[this.props.history.length - 1].squares[i]}
+    onClick={() => this.props.onClick(i)}
     />
     );
   }
 
-  handleClick(i: number) {
-    const squares = this.state.squares.slice();
-    if ((calculateWinner(squares) || squares[i]) !== '') {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext
-    });
-  }
-
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status: string;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -79,15 +53,60 @@ class Board extends React.Component<BoardProps, {squares: string[], xIsNext: boo
   }
 }
 
-class Game extends React.Component {
+interface GameProps {}
+
+interface GameState {
+  history: Array<{squares: string[]}>, 
+  xIsNext: boolean
+}
+
+class Game extends React.Component<GameProps, GameState> {
+  constructor(props: GameProps) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill('')
+      }],
+      xIsNext: true
+    }
+  }
+
+  handleClick(i: number) {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if ((calculateWinner(squares) || squares[i]) !== '') {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{
+        squares: squares,
+      }]),
+      xIsNext: !this.state.xIsNext
+    });
+  }
+
   render() {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+    let status: string;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board 
+          history={history}
+          onClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
